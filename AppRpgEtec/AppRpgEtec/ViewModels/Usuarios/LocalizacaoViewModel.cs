3da3294ec00ce93f9dde20eb275868925ab8cd32
unknown <rogeriozfaga@gmail.com>
2025-09-08 08:54:48 -0300

    aula 7

---
+++ AppRpgEtec/AppRpgEtec/ViewModels/Usuarios/LocalizacaoViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    namespace AppRpgEtec.ViewModels.Usuarios;
+
+    using Microsoft.Maui.Controls.Maps;
+    using Microsoft.Maui.Maps;
+    using Map = Microsoft.Maui.Controls.Maps.Map;
+
+    private Map meuMapa;
+
+    public Map MeuMapa
+    {
+        get => meuMapa;
+        set
+        {
+            if (value != null)
+            {
+                meuMapa = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public async void InicializarMapa()
+    {
+        try
+        {
+            Location location = new Location(-23.5200241d, -46.596498d);
+            Pin pinEtec = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Etec Horácio",
+                Address = "Rua alcântara, 113, Vila Guilherme",
+                Location = location
+            };
+
+            Map map = new Map();
+            MapSpan mapSpan = MapSpan
+                .FromCenterAndRadius(location, Distance.FromKilometers(5));
+            map.Pins.Add(pinEtec);
+            map.MoveToRegion(mapSpan);
+
+            MeuMapa = map;
+        }
+        catch (Exception ex)
+        {
+            await Application.Current.MainPage
+                    .DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
+}
---
+++ AppRpgEtec/AppRpgEtec/Views/Usuarios/LocalizacaoView.xaml.cs
@@ -0,0 +1,10 @@
+namespace AppRpgEtec.Views.Usuarios;
+
+public partial class LocalizacaoView : ContentPage
+{
+	LocalizacaoViewModel viewModel;
+	public LocalizacaoView()
+	{
+		InitializeComponent();
+	}
+}
