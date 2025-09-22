a9288bd628d8928c53e9974baa25b2b7d80e835a
RogerioZFaga <rogeriozfaga@gmail.com>
2025-09-22 08:55:50 -0300

    dia 22/09

---
+++ ATIVIDADE AVALIATIVA DO DIA 22.09.2025/AppRpgEtec/AppRpgEtec/AppRpgEtec/Services/Enderecos/EnderecoService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+using Newtonsoft.Json.Linq;
+
+namespace AppRpgEtec.Services.Enderecos
+{
+    internal class EnderecoService : Request
+    {
+        private readonly Request _request;
+        private const string _apiUrlBase = "https://rpgapi3ai2025.azurewebsites.net/Enderecos";
+        //Azure: https://rpgapi3ai2025.azurewebsites.net/Enderecos
+        //Somee: http://luizfernando.somee.com/RpgApi/Enderecos
+
+        public EnderecoService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<ObservableCollection<Endereco>> GetEnderecoAsync (string name)
+        {
+            string urlComplementar = string.Format("{0}", name);
+            ObservableCollection<Models.Endereco> listaEnderecos = await
+            _request.GetAsync<ObservableCollection<Models.Endereco>>(_apiUrlBase, urlComplementar);
+            return listaEnderecos;
+
+        }
+    }
+}
